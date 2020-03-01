e4c93b21481e2e7185bbe27ad6f747a088515e46
baraquda <baraquda2012@yandex.ru>
2020-03-02 02:39:25 +0300

    optimized for mobile version

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction(y) {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+  y.classList.toggle("change");
+}
