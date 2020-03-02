73eb82ac555f5e4d303c51ffcc7f364014c923ab
baraquda <baraquda2012@yandex.ru>
2020-03-02 17:00:26 +0300

    added onclick for button

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function myFunction(y) {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-  y.classList.toggle("change");
+function myFunc() {
+  alert("Очень скоро все наладится");
 }
