ce5c5d277304179494b62734326a5fc70351b01e
baraquda <baraquda2012@yandex.ru>
2020-03-02 21:17:23 +0300

    fixed bug

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function myFunc() {
   alert("Очень скоро все наладится");
 }
+function myFunction(y){
+	var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+  y.classList.toggle("change");
+}
